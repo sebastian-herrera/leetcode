1fa4e4c55eda6b72466d3a32cfc610ba138d8871
Sebastian Herrera <herrera6911sebastian@hotmail.com>
2026-03-17 17:04:29 -0500

    sol(104): Maximum Depth of Binary Tree

--- typescript/problem_0104/MaximumDepthOfBinaryTree.ts
+++ typescript/problem_0104/MaximumDepthOfBinaryTree.ts
@@ -22,3 +22,21 @@ export function maxDepth(root: TreeNode | null): number {
 
   return Math.max(l, r) + 1;
 }
+
+// // iterative way
+// export function maxDepth(root: TreeNode | null): number {
+//   if (!root) return 0;
+
+//   const stack: [TreeNode, number][] = [[root, 1]];
+//   let ans = 0;
+
+//   while (stack.length) {
+//     const [node, depth] = stack.pop()!;
+//     ans = Math.max(ans, depth);
+
+//     if (node.left) stack.push([node.left, depth + 1]);
+//     if (node.right) stack.push([node.right, depth + 1]);
+//   }
+
+//   return ans;
+// }
