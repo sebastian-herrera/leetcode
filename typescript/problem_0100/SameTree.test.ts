b4c972c14c4cf8eecc601d84c5d84cb42a42cea8
Sebastian Herrera <herrera6911sebastian@hotmail.com>
2026-03-18 18:02:43 -0500

    sol(100): Same Tree

---
+++ typescript/problem_0100/SameTree.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from 'vitest';
+import { arrayToTree } from '../common/tree.ts';
+import { isSameTree } from './SameTree.ts';
+
+test('isSameTree', () => {
+  expect(isSameTree(arrayToTree([1, 2, 3]), arrayToTree([1, 2, 3]))).toBe(true);
+  expect(isSameTree(arrayToTree([1, 2]), arrayToTree([1, null, 2]))).toBe(false);
+  expect(isSameTree(arrayToTree([1, 2, 1]), arrayToTree([1, 1, 2]))).toBe(false);
+});
