28be2754c9d380f7c69c57a0be58c7ae94df463c
Sebastian Herrera <herrera6911sebastian@hotmail.com>
2026-04-16 15:30:45 -0500

    sol(841) Keys and Rooms
    iterative

--- typescript/problem_0841/KeysAndRooms.ts
+++ typescript/problem_0841/KeysAndRooms.ts
@@ -13,3 +13,22 @@ export function canVisitAllRooms(rooms: number[][]): boolean {
 
   return seen.size === rooms.length;
 }
+
+// iterative
+// export function canVisitAllRooms(rooms: number[][]): boolean {
+//   const stack = [0];
+//   const seen = new Set([0]);
+
+//   while (stack.length) {
+//     const node = stack.pop()!;
+
+//     for (const neighbor of rooms[node]) {
+//       if (!seen.has(neighbor)) {
+//         seen.add(neighbor);
+//         stack.push(neighbor);
+//       }
+//     }
+//   }
+
+//   return seen.size === rooms.length;
+// }
